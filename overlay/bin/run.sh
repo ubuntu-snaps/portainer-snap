7ab39f04d992f1122490176b43b521fcc4d709b4
lvming <lvming@restartech.com>
2025-08-21 13:21:27 +0800

    feat: basic portainer snap

---
+++ overlay/bin/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NAME="$SNAP_INSTANCE_NAME"
+IMAGE=$(snapctl get image)
+
+if [ ! "$(docker ps --all --quiet --filter name="$NAME")" ]; then
+    echo "Container does not exist. Creating ..."
+    docker volume create portainer_data
+    docker create \
+        --name "$NAME" \
+        --restart=always \
+        -p 9443:9443 \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v portainer_data:/data \
+        "$IMAGE"
+fi
+
+echo "Starting container ..."
+docker start --attach "$NAME"
